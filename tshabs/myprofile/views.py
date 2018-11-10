da1dd3f8e732eb55f74a936102545bd6a8066e77
shaynetshabs <nathietshabs@gmail.com>
2018-11-10 09:15:53 +0200

    switched to base.html

--- tshabs/myprofile/views.py
+++ tshabs/myprofile/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    temp_path = 'myprofile/home.html'
+    temp_path = 'myprofile/base.html'
     temp_dict = {'title': home}
     return render(request, temp_path, temp_dict)
